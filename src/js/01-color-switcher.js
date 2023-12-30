a443b90fbb5cd5762c4cef2b73b3035a621d46cd
LaurenEnclona <137889742+LaurenEnclona@users.noreply.github.com>
2023-12-30 21:10:01 +0800

    task 1 2 and 3

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+
+let intervalId;
+
+function changeBackgroundColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+startButton.addEventListener('click', () => {
+  intervalId = setInterval(changeBackgroundColor, 1000);
+  startButton.disabled = true; // Disable Start button while running
+});
+
+stopButton.addEventListener('click', () => {
+  clearInterval(intervalId);
+  startButton.disabled = false; // Enable Start button when stopped
+});
